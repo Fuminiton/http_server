ff238efcd29a724858feea80d4a61beb8201209e
fuminiton <fumi_8823@icloud.com>
2025-11-15 16:13:54 +0900

    create: client

--- client.c
+++ client.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <errno.h>
 #include <stdlib.h>
@@ -6,3 +7,67 @@
 #include <arpa/inet.h>
 #include <unistd.h>
 #include <netdb.h>
+
+#define MAX_CONNECTION 3
+#define MAX_BUFFER_SIZE 1024
+
+void error(char *msg) {
+    fprintf(stderr, "%s: %s\n", msg, strerror(errno));
+    exit(1);
+}
+
+int tcp_connect(const char *hostname, int port) {
+    int socket_fd;
+    struct sockaddr_in addr;
+    int return_code;
+    struct hostent *host;
+
+    socket_fd = socket(
+        AF_INET,
+        SOCK_STREAM,
+        IPPROTO_TCP
+    );
+    if (socket_fd < 0) {
+        error("Creating socket failed");
+    }
+
+    bzero((char *) &addr, sizeof(addr));
+    addr.sin_family = AF_INET;
+    host = gethostbyname(hostname);
+    addr.sin_addr = *(struct in_addr *) (host->h_addr_list[0]);
+    addr.sin_port = htons(port);
+    
+    return_code = connect(
+        socket_fd,
+        (struct sockaddr *) &addr,
+        sizeof(addr)
+    );
+    if (return_code < 0) {
+        close(socket_fd);
+        error("Connecting failed");
+    }
+
+    return socket_fd;
+}
+
+int main(int argc, char* argv[]) {
+    int socket_fd;
+    char *input;
+    char buf[MAX_BUFFER_SIZE];
+    char* hostname;
+    int port;
+    
+    if (argc != 3) {
+        printf("usage: %s [ip address] [port]\n", argv[0]);
+    }
+
+    hostname = argv[1];
+    port = atoi(argv[2]);
+
+    socket_fd = tcp_connect(hostname, port);
+    puts("Waiting for your standard input...");
+    input = fgets(buf, MAX_BUFFER_SIZE, stdin);
+    write(socket_fd, input, strlen(input));
+    close(socket_fd);
+    return 0;
+}
