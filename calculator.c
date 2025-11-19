ef1b73fbd52d392d62695e79b13e9bb5b90854fb
fuminiton <fumi_8823@icloud.com>
2025-11-20 00:39:26 +0900

    refactoring: server.c

--- calculator.c
+++ calculator.c
@@ -1,51 +1,218 @@
+#include <ctype.h>
+#include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdarg.h>
+#include <stdbool.h>
 
 
-int calculate(char *buf) {
-    int num1, num2;
-    char op;
+/*
+14+(2+13)+4*2
+tokenize
+parse
+calculate
+*/
 
-    sscanf(buf, "%d%c%d", &num1, &op, &num2);
-    switch (op) {
-    case '+':
-        return num1 + num2;
-    case '-':
-        return num1 - num2;
-    case '*':
-        return num1 * num2;
-    case '/':
-        if (num2 == 0) {
-            puts("error: you can't use 0 for division.");
-            break;
-        }
-        return num1 / num2;
-    default:
-        break;
-    }
+typedef enum {
+    TK_RESERVED,  // Symbol
+    TK_NUM,       // Integer token
+    TK_EOF,       // End-of-File token
+} TokenKind;
 
-    return 0;
+typedef enum {
+    ND_ADD,       // +
+    ND_SUB,       // -
+    ND_MUL,       // *
+    ND_DIV,       // /
+    ND_NUM,       // number
+} NodeKind;
+
+typedef struct Token Token;
+typedef struct Node Node;
+
+struct Token {
+    TokenKind kind;
+    Token *next;
+    int val;
+    char *str;
+};
+
+struct Node {
+    NodeKind kind;
+    Node *lhs;
+    Node *rhs;
+    int val;
+};
+
+char *user_input;
+Token *token;
+
+void error(char *fmt, ...) {
+    va_list ap;
+    va_start(ap, fmt);
+    vfprintf(stderr, fmt, ap);
+    fprintf(stderr, "\n");
+    exit(1);
 }
 
-int main() {
-    char buf[1024];
+void error_at(char *loc, char *fmt, ...) {
+  va_list ap;
+  va_start(ap, fmt);
 
-    strcpy(buf, "1+1");
-    int answer = calculate(buf);
-    printf("%s is %d\n", buf, answer);
+  int position = loc - user_input;
+  fprintf(stderr, "%s\n", user_input);
+  fprintf(stderr, "%*s", position, " ");
+  fprintf(stderr, "^ ");
+  vfprintf(stderr, fmt, ap);
+  fprintf(stderr, "\n");
+  exit(1);
+}
 
-    strcpy(buf, "5-2");
-    answer = calculate(buf);
-    printf("%s is %d\n", buf, answer);
+Token *new_token(TokenKind kind, Token *current_token, char *str) {
+    Token *new_token = calloc(1, sizeof(Token));
+    new_token->kind = kind;
+    new_token->str = str;
+    current_token->next = new_token;
+    return new_token;
+}
 
-    strcpy(buf, "2*6");
-    answer = calculate(buf);
-    printf("%s is %d\n", buf, answer);
+Token *tokenize() {
+    char *p = user_input;
+    Token head;
+    head.next = NULL;
+    Token *current_token = &head;
 
-    strcpy(buf, "12/3");
-    answer = calculate(buf);
-    printf("%s is %d\n", buf, answer);
+    while (*p) {
+        if (isspace(*p)) {
+            p++;
+            continue;
+        }
+        if (strchr("+-*/()", *p)) {
+            current_token = new_token(TK_RESERVED, current_token, p);
+            p++;
+            continue;
+        }
+        if (isdigit(*p)) {
+            current_token = new_token(TK_NUM, current_token, p);
+            current_token->val = strtol(p, &p, 10);
+            continue;
+        }
+
+        error_at(p, "Invalid token");
+    }
+    new_token(TK_EOF, current_token, p);
+    return head.next;
+}
+
+
+bool consume(char op) {
+    if (token->kind != TK_RESERVED || token->str[0] != op) {
+        return false;
+    }
+    token = token->next;
+    return true;
+}
+
+void expect(char op) {
+    if (token->kind != TK_RESERVED || token->str[0] != op) {
+        error_at(token->str, "expected '%c'", op);
+    }
+    token = token->next;
+}
+
+int expect_number() {
+    if (token->kind != TK_NUM) {
+        error_at(token->str, "expected a number");
+    }
+    int num = token->val;
+    token = token->next;
+    return num;
+}
+
+bool at_eof() {
+    return token->kind == TK_EOF;
+}
+
+Node *new_node(NodeKind kind, Node *lhs, Node *rhs) {
+    Node *node = calloc(1, sizeof(Node));
+    node->kind = kind;
+    node->lhs = lhs;
+    node->rhs = rhs;
+    return node;
+}
+
+Node *new_node_num(int num) {
+    Node *node = calloc(1, sizeof(Node));
+    node->kind = ND_NUM;
+    node->val = num;
+    return node;
+}
+
+Node *expr();
+Node *mul();
+Node *unary();
+Node *primary();
+
+Node *expr() {
+    Node *node = mul();
+    for (;;) {
+        if (consume('+')) {
+            node = new_node(ND_ADD, node, mul());
+            continue;
+        }
+        if (consume('-')) {
+            node = new_node(ND_SUB, node, mul());
+            continue;
+        }
+        return node;
+    }
+}
+
+Node *mul() {
+    Node *node = unary();
+    for (;;) {
+        if (consume('*')) {
+            node = new_node(ND_MUL, node, unary());
+            continue;
+        }
+        if (consume('/')) {
+            node = new_node(ND_DIV, node, unary());
+            continue;
+        }
+        return node;
+    }
+}
+
+Node *unary() {
+    if (consume('-')) {
+        return new_node(ND_DIV, new_node_num(0), primary());
+    }
+    return primary();
+}
+
+Node *primary() {
+    if (consume('(')) {
+        Node *node = expr();
+        consume(')');
+        return node;
+    }
+    return new_node_num(expect_number());
+}
+
+void gen(Node* node) {
+}
+
+
+int main(int argc, char* argv[]) {
+    int answer;
+    user_input = argv[1];
+    token = tokenize();
+    Node *node = expr();
+
+    //answer = evaluator(node);
+
+    //printf("buf = %d\n", answer);
 
     return 0;
 }
--- server.c
+++ server.c
@@ -1,46 +1,53 @@
 #include <stdio.h>
 #include <string.h>
-#include <errno.h>
 #include <stdlib.h>
 #include <sys/socket.h>
 #include <arpa/inet.h>
 #include <unistd.h>
 
+#define PORT 11111
 #define MAX_CONNECTION 3
 #define MAX_BUFFER_SIZE 1024
 
-void error(char *msg) {
-    fprintf(stderr, "%s: %s\n", msg, strerror(errno));
-    exit(1);
-}
 
 int tcp_listen(int port) {
     int socket_fd;
     struct sockaddr_in addr;
     int return_code;
-    int reuse_option = 1;   // 1: valid
+    int opt = 1;   // 1: valid
 
+    // create socket
     socket_fd = socket(
         AF_INET,
         SOCK_STREAM,
         0
     );
     if (socket_fd < 0) {
-        error("Creating socket failed");
+        fprintf(stderr, "Creating socket failed.\n");
+        exit(1);
     }
 
+    // set address
     bzero((char *) &addr, sizeof(addr));
     addr.sin_family = AF_INET;
     addr.sin_addr.s_addr = htonl(INADDR_ANY);
     addr.sin_port = htons(port);
-    setsockopt(
+
+    // set socket options
+    return_code = setsockopt(
         socket_fd,
         SOL_SOCKET,
         SO_REUSEADDR,
-        &reuse_option,
-        sizeof(reuse_option)
+        &opt,
+        sizeof(opt)
     );
-    
+    if (return_code < 0) {
+        close(socket_fd);
+        fprintf(stderr, "Setting socket options failed.\n");
+        exit(1);
+    }
+
+    // bind
     return_code = bind(
         socket_fd,
         (struct sockaddr *) &addr,
@@ -48,52 +55,66 @@ int tcp_listen(int port) {
     );
     if (return_code < 0) {
         close(socket_fd);
-        error("Binding failed");
+        fprintf(stderr, "Binding failed.\n");
+        exit(1);
     }
 
+    // listen
     return_code = listen(
         socket_fd,
         MAX_CONNECTION
     );
     if (return_code < 0) {
         close(socket_fd);
-        error("Listening failed");   
+        fprintf(stderr, "Listening failed.\n");
+        exit(1);   
     }
 
     return socket_fd;
 }
 
-int main(int argc, char* argv[]) {
-    int listen_fd;
+int connect_client(int listen_fd) {
     int connect_fd;
     struct sockaddr_storage client_addr;
     unsigned int client_addr_size = sizeof(client_addr);
-    int return_code;
-    char buf[MAX_BUFFER_SIZE];
-    
-    listen_fd = tcp_listen(11111);
-    puts("Waiting for connection...");
+
     connect_fd = accept(
         listen_fd,
         (struct sockaddr *) &client_addr,
         &client_addr_size
     );
     if (connect_fd < 0) {
-        close(listen_fd);
-        error("Connecting a client failed");
+        fprintf(stderr, "Connecting a client failed\n");
     }
 
-    return_code = recv(connect_fd, buf, MAX_BUFFER_SIZE, 0);
+    return connect_fd;
+}
+
+void handle_client(int connect_fd) {
+    char* buf[MAX_BUFFER_SIZE];
+    int return_code = recv(connect_fd, buf, MAX_BUFFER_SIZE, 0);
     if (return_code < 0) {
         close(connect_fd);
-        close(listen_fd);
-        error("Recv failed");
     }
-
     write(1, buf, return_code);
     return_code = send(connect_fd, buf, return_code, 0);
-
     close(connect_fd);
+}
+
+int main(int argc, char* argv[]) {
+    int listen_fd;
+    int connect_fd;
+   
+    listen_fd = tcp_listen(PORT);
+    printf("Server is running on port %d...\n", PORT);
+
+    while (1) {
+        connect_fd = connect_client(listen_fd);
+        if (connect_fd < 0) {
+            continue;
+        }
+        handle_client(connect_fd);
+    }
     close(listen_fd);
 
     return 0;
